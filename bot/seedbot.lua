c2cc566f160288d25a989d0a48840ca70f84318f
rezaf23 <rezamehrdad1372@gmail.com>
2016-07-08 00:50:44 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {@mrrezatabriz1372},--Sudo users
+    sudo_users = {241783797},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
